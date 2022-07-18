03798cc4a21f2879af282710198d77d143166f9b
Verena Lechner <v.m.s.lechner@gmail.com>
2022-07-18 22:05:55 +0200

    readme

--- suffixGenerator/suffixData.go
+++ suffixGenerator/suffixData.go
@@ -1,6 +1,6 @@
 package suffixGenerator
 
 var (
-	ADJECTIVES = []string{"brave", "lazy", "bitter", "misty", "silent", "funny", "crazy", "d"}
-	ROLES      = []string{"mountaineer", "gardener", "athlete", "rain-dancer", "sailor", "water bender", "wave rider", "rockstar"}
+	ADJECTIVES = []string{"brave", "lazy", "bitter", "misty", "silent", "funny", "crazy", "super"}
+	ROLES      = []string{"mountaineer", "gardener", "athlete", "rain-dancer", "sailor", "water bender", "wave rider", "rockstar", "hero"}
 )
